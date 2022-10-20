11cebe5c7f8c2c91a390802ea81815e29a974b31
Annie <annie55079@gmail.com>
2022-10-20 23:55:14 +0800

    Initial Commit

---
+++ ios_Hw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_Hw2
+//
+//  Created by Yu Zhi on 2022/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_Hw2/ios_Hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_Hw2App.swift
+//  ios_Hw2
+//
+//  Created by Yu Zhi on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ios_Hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
